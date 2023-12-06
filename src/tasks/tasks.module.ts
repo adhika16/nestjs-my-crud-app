c8fcf25d1a5f18db9d90ff4a0014ae94d56a5e73
Adhika P <40765693+adhika16@users.noreply.github.com>
2023-12-06 13:00:49 +0700

    enable authentication globally on Tasks module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,12 +4,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TaskSchema } from './task.schema';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core/constants';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema }]),
   ],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [
+    TasksService, 
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class TasksModule {}
